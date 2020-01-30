8bb44990470216a716ffac2dc59798a55f1b23ff
dunkelweizen <53185634+dunkelweizen@users.noreply.github.com>
2020-01-29 22:53:52 -0500

    assignment1 work

---
+++ module1-introduction-to-sql/assignment_1_pt1.py
@@ -0,0 +1,62 @@
+import sqlite3
+#DB_FILEPATH = os.path.join(os.path.dirname(__file__), '..', "data", "rpg_db.sqlite3")
+
+conn = sqlite3.connect("rpg_db.sqlite3")
+# h/t: https://kite.com/python/examples/3884/sqlite3-use-a-row-factory-to-access-values-by-column-name
+conn.row_factory = sqlite3.Row
+curs = conn.cursor()
+query = """
+SELECT COUNT(name) FROM charactercreator_character;
+"""
+results = curs.execute(query).fetchall()
+print("Total characters:", results[0]['COUNT(name)'])
+
+subclass = ['charactercreator_cleric', 'charactercreator_fighter', 'charactercreator_mage', 'charactercreator_thief']
+for character_class in subclass:
+    query = f"SELECT COUNT(character_ptr_id) FROM {character_class}"
+    results = curs.execute(query).fetchall()
+    print(f'Characters in class {character_class[17:]}:', results[0]['COUNT(character_ptr_id)'])
+
+query = "SELECT COUNT(mage_ptr_id) FROM charactercreator_necromancer"
+
+results = curs.execute(query).fetchall()
+print('Characters in class necromancer:', results[0]['COUNT(mage_ptr_id)'])
+
+query = "SELECT COUNT(item_id) FROM armory_item"
+results = curs.execute(query).fetchall()
+
+print(results[0]['COUNT(item_id)'])
+items = results[0]['COUNT(item_id)']
+
+query = "SELECT COUNT(item_ptr_id) FROM armory_weapon"
+weapons = (curs.execute(query).fetchall())[0]['COUNT(item_ptr_id)']
+print('Weapons =', weapons)
+print('Items which are not weapons =', (items - weapons))
+
+query = """SELECT
+character_id,
+COUNT(item_id)
+FROM
+    charactercreator_character_inventory
+GROUP BY
+    character_id
+LIMIT
+    20;"""
+results = curs.execute(query).fetchall()
+for i in range(len(results)):
+    print('Character ID', results[i][0], 'has', results[i][1], 'items.')
+
+query = """
+SELECT
+character_id,
+COUNT(item_id)
+FROM
+    (charactercreator_character_inventory JOIN armory_weapon ON charactercreator_character_inventory.item_id = armory_weapon.item_ptr_id)
+GROUP BY
+    character_id
+LIMIT
+    20;"""
+results = curs.execute(query).fetchall()
+for i in range(len(results)):
+    print('Character ID', results[i][0], 'has', results[i][1], 'weapons')
+
---
+++ module1-introduction-to-sql/assignment_1_pt2.py
@@ -0,0 +1,23 @@
+import sqlite3
+import pandas as pd
+import warnings
+warnings.simplefilter(action='ignore', category=UserWarning)
+
+data = pd.read_csv('buddymove_holidayiq.csv')
+
+conn = sqlite3.connect("buddymove_holidayiq.sqlite3")
+data.to_sql('review', conn, if_exists = 'replace')
+curs = conn.cursor()
+query = "SELECT * FROM review"
+
+results = curs.execute(query).fetchall()
+print("There are", len(results), "rows")
+
+query = """
+SELECT COUNT('User Id') FROM review
+WHERE Nature >= 100
+AND Shopping >= 100;
+"""
+
+results = curs.execute(query).fetchall()
+print('There are', results[0][0], 'users who rated both Nature and Shopping over 100')
