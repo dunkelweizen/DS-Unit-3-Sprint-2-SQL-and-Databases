a57c5f7dc1c0a53942afcf757da8c0390dfb6e61
dunkelweizen <53185634+dunkelweizen@users.noreply.github.com>
2020-02-06 22:05:01 -0500

    finding married couples in titanic database

--- module4-acid-and-database-scalability-tradeoffs/married_couples.py
+++ module4-acid-and-database-scalability-tradeoffs/married_couples.py
@@ -1,29 +1,19 @@
 import pandas as pd
+import numpy as np
 
 titanic = pd.read_csv(
     'https://raw.githubusercontent.com/dunkelweizen/DS-Unit-3-Sprint-2-SQL-and-Databases/master/module2-sql-for-analysis/titanic.csv')
 
 titanic['Mr_Mrs'] = titanic['Name'].apply(lambda x: True if 'Mr' in x else False)
+mr_mrs = titanic[titanic['Mr_Mrs']]
+paired_names = pd.DataFrame()
+for row in mr_mrs['Name']:
+    last_5_chars = row[-5:]
+    paired_names[last_5_chars] = titanic['Name'].apply(lambda x:x if last_5_chars in x else np.NaN)
+
+for column in paired_names.columns:
+    if paired_names['column'].nunique() > 1:
+        print(paired_names[column].max())
 
 
-def common_substring(column):
-    array = column.to_numpy(dtype=str)
-    n = len(array)
-    s = array[0]
-    l = len(s)
-    res = ''
-    for i in range(l):
-        for j in range(i + 1, l + 1):
-            stem = s[i:j]
-            k = 1
-            for k in range(1, n):
-                if stem not in array[k]:
-                    break
-            if (k + 1 == n and len(res) < len(stem)):
-                res = stem
-    return stem
-
-
-print(common_substring(titanic['Mr_Mrs']))
-#  that is not what I wanted...
-#  more sprint practice on SQL and databases and I'll come back to this
+#  take a break and go study databases and then come back
